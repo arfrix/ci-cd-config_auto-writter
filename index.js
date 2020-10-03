9f07e6a0081178f869a3d41cb91349437b82f793
arfa <arfrix.dev@gmail.com>
2020-10-03 16:51:18 +0330

    refactored and some file added

--- index.js
+++ index.js
@@ -2,37 +2,47 @@ const Handlebars = require("handlebars");
 var fs = require('fs');
 const prompt = require('prompt');
 
+
+const files = [
+  {tamplateName: 'production-config.template', whereToWrite: 'docker-deploy/production/config.json'},
+  {tamplateName: 'production-docker-compose.template', whereToWrite: 'docker-deploy/production/docker-compose.yml'},
+  {tamplateName: 'production-env.template', whereToWrite: 'docker-deploy/production/env.json'},
+  {tamplateName: 'staging-config.template', whereToWrite: 'docker-deploy/staging/config.json'},
+  {tamplateName: 'staging-docker-compose.template', whereToWrite: 'docker-deploy/staging/docker-compose.yml'},
+  {tamplateName: 'staging-env.template', whereToWrite: 'docker-deploy/staging/env.json'},
+  
+  {tamplateName: 'docker.template', whereToWrite: 'Dockerfile'},
+]
+
 prompt.start();
-prompt.get(['version', 'file'], function (err, result) {
+prompt.get(['version', 'file'], function (err, userInput) {
     if (err) { return onErr(err); }
-    render(result)
+    writter(userInput)
 });
 
-function render(results){
+function writter(results){
     
-fs.readFile('gitlab-ci.yml.template', function(err, data){
-    if (!err) {
-      // make the buffer into a string
-      var source = data.toString();
-      // call the render function
-      version = console.read
-      renderToString(source, { version: results.version,file:results.file });
-      // create and write file
-      fs.writeFile('test/neewfile.txt', renderToString(source, { version: results.version,file:results.file }), function (err) {
-        if (err) throw err;
-        console.log('File is created successfully.');
-      }); 
+  fs.readFile('docker.template', function(err, data){
+      if (!err) {
+        // make the buffer into a string
+        var source = data.toString();
+        version = console.read
+        
+        // create and write file
+        fs.writeFile('neewfile.txt', renderToString(source, { version: results.version,file:results.file }), function (err) {
+          if (err) throw err;
+          console.log('File is created successfully.');
+        }); 
 
-    } else {
-      // handle file read error
-      console.log("Error")
-    }
-  });
+      } else {
+        // handle file read error
+        console.log("Error")
+      }
+    });
 }
-function renderToString(source, data) {
+function dataWeWantToWrite(source, data) {
     var template = Handlebars.compile(source);
     var outputString = template(data);
-    console.log(outputString)
     return outputString;
 }
     
